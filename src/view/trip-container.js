0ab303ac91927bbb0decebc766650c092cd1c534
jackkk94 <jackpanchenko@mail.ru>
2020-08-17 22:41:50 +0300

    разбиение на компоненты

---
+++ src/view/trip-container.js
@@ -0,0 +1,5 @@
+export const createTripContainerTemplate = () => {
+  return (
+    `<ul class="trip-days"></ul>`
+  );
+};
---
+++ src/view/trip-day-container.js
@@ -0,0 +1,8 @@
+export const createTripDayContainerTemplate = () => {
+  return (
+    `<li class="trip-days__item  day">
+        <ul class="trip-events__list">
+        </ul>
+    </li>`
+  );
+};
